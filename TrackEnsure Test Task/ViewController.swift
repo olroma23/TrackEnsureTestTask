f669ee20bad9839aa4bd5259cdc0dd63977c6f1a
olroma23 <roliynyk68@gmail.com>
2020-08-28 17:53:34 +0300

    Initial Commit

---
+++ TrackEnsure Test Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackEnsure Test Task
+//
+//  Created by Roman Oliinyk on 28.08.2020.
+//  Copyright © 2020 Roman Oliinyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
